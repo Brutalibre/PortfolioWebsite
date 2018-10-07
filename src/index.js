5adfc059bec39345f62a72064dd1e8d01176b3ce
Val Isautier <val.isautier@gmail.com>
2018-10-07 21:13:35 +0200

    Started website with webpack-starter-basic.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+require('normalize.css/normalize.css');
+require('./styles/index.scss');
+require('./styles/parallax.scss');
+
+/*document.addEventListener("DOMContentLoaded", () => {
+
+    const pluginsTriggerElement = document.getElementById('plugins-trigger');
+    const pluginsElement = document.getElementById('plugins');
+
+    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+
+    pluginsTriggerElement.onclick = () => {
+        pluginsElement.classList.toggle(pluginsVisibleClass);
+    }
+});*/
+
+document.getElementById("DownloadCV").onclick = () => {
+    console.log("clicked!")
+}
