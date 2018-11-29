ad5a3ab0405b6684e2dcacd9a1af1a6c0bc2c3f7
Val Isautier <val.isautier@gmail.com>
2018-11-29 21:20:56 +0100

    Added page for Noche de Alebrijes + config file for release.

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,16 @@ const buildPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
     devtool: 'source-map',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        panem: './src/panem/panem.js',
+        gray: './src/gray/gray.js',
+        doom: './src/doom/doom.js',
+        nap: './src/nap/nap.js',
+        noche: './src/noche/noche.js',
+        esd: './src/esd/esd.js',
+        peak: './src/peak/peak.js'
+    },
     output: {
         filename: '[name].[hash:20].js',
         path: buildPath
@@ -74,9 +83,52 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html',
-            // Inject the js bundle at the end of the body of the given template
-            inject: 'body',
+            template: './src/index.html',
+            inject: true,
+            chunks: ['index'],
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/panem/panem.html',
+            inject: true,
+            chunks: ['panem'],
+            filename: 'panem.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/gray/gray.html',
+            inject: true,
+            chunks: ['gray'],
+            filename: 'gray.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/doom/doom.html',
+            inject: true,
+            chunks: ['doom'],
+            filename: 'doom.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/nap/nap.html',
+            inject: true,
+            chunks: ['nap'],
+            filename: 'nap.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/noche/noche.html',
+            inject: true,
+            chunks: ['noche'],
+            filename: 'noche.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/esd/esd.html',
+            inject: true,
+            chunks: ['esd'],
+            filename: 'esd.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/peak/peak.html',
+            inject: true,
+            chunks: ['peak'],
+            filename: 'peak.html'
         }),
         new CleanWebpackPlugin(buildPath),
         new FaviconsWebpackPlugin({
